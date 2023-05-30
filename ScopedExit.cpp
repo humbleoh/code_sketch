2fff74d807ad99d6f4d62122352996d55d513db0
Yu Sheng Oh <yusheng.oh2@dyson.com>
2023-05-30 23:36:48 +0800

    1. Add noexcept operator to enable/disable noexcept specification to dtor. 2. Use C++20's concept to constraint typename T to be a callable type.

--- ScopedExit.cpp
+++ ScopedExit.cpp
@@ -1,17 +1,20 @@
+#include <concepts>
+#include <functional>
 #include <iostream>
 
-#define SCOPED_EXIT(f)  SCOPED_EXIT_UNIQ(f, __LINE__)
-#define SCOPED_EXIT_UNIQ(f, l)  ScopedExit scopedExit__##l { f }
+#define SCOPED_EXIT(f)  SCOPED_EXIT_UNIQ((f), __LINE__)
+#define SCOPED_EXIT_UNIQ(f, l)  SCOPED_EXIT_UNIQ_EXPAND(f, l)
+#define SCOPED_EXIT_UNIQ_EXPAND(f, l)  ScopedExit scopedExit__##l { (f) }
 
-template<typename T>
+template<std::invocable T>
 class ScopedExit {
 public:
-    ScopedExit(T f)
-        : m_f { f }
+    ScopedExit(T &&f)
+        : m_f { std::forward<T>(f) }
     { }
 
     // https://en.cppreference.com/w/cpp/language/as_if
-    ~ScopedExit() noexcept
+    ~ScopedExit() noexcept(noexcept(std::is_nothrow_invocable_v<T>))
     {
         m_f();
     }
@@ -20,28 +23,17 @@ private:
     T m_f;
 };
 
-class TestClass {
-public:
-    TestClass()
-    {
-        std::cout << "testclass ctor" << std::endl;
-    }
-
-    ~TestClass()
-    {
-        std::cout << "testclass dtor" << std::endl;
-    }
-
-
-    void Close()
-    {
-        std::cout << "close" << std::endl;
-    }
-};
-
 int main(int argc, const char *argv[])
 {
-    TestClass t;
-    SCOPED_EXIT([&](){ t.Close(); });
+    int *ptrInt = new int(10);
+    SCOPED_EXIT([&]() {
+        std::cout << "delete ptr, *ptr=" << *ptrInt << std::endl;
+        delete ptrInt;
+    });
+    
+    std::function<void()> test2 = []() { std::cout << "test2" << std::endl; };
+    SCOPED_EXIT(test2);
+    
+    return 0;
 }
 
