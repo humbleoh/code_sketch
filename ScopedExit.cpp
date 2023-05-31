c8835393563999899eb578a5152b7a837960e348
Yu Sheng Oh <yusheng.oh2@dyson.com>
2023-05-31 11:37:52 +0800

    Fix compile issue.

--- ScopedExit.cpp
+++ ScopedExit.cpp
@@ -2,26 +2,32 @@
 #include <functional>
 #include <iostream>
 
-#define SCOPED_EXIT(f)  SCOPED_EXIT_UNIQ((f), __LINE__)
+#define SCOPED_EXIT(f)  SCOPED_EXIT_UNIQ(f, __LINE__)
 #define SCOPED_EXIT_UNIQ(f, l)  SCOPED_EXIT_UNIQ_EXPAND(f, l)
-#define SCOPED_EXIT_UNIQ_EXPAND(f, l)  ScopedExit scopedExit__##l { (f) }
+#define SCOPED_EXIT_UNIQ_EXPAND(f, l)  ScopedExit CONCAT_NAME(scopedExit, l) { f }
+#define CONCAT_NAME(n1, n2)  n1 ## n2
 
-template<std::invocable T>
+template<std::invocable Callable>
 class ScopedExit {
 public:
-    ScopedExit(T &&f)
-        : m_f { std::forward<T>(f) }
+    ScopedExit(Callable f)
+        : m_f { std::move(f) }
     { }
 
     // https://en.cppreference.com/w/cpp/language/as_if
-    ~ScopedExit() noexcept(noexcept(std::is_nothrow_invocable_v<T>))
+    ~ScopedExit() noexcept(noexcept(std::is_nothrow_invocable_v<Callable>))
     {
         m_f();
     }
 
 private:
-    T m_f;
+    Callable m_f;
 };
+
+void test3()
+{
+    std::cout << "test3" << std::endl;
+}
 
 int main(int argc, const char *argv[])
 {
@@ -33,6 +39,8 @@ int main(int argc, const char *argv[])
     
     std::function<void()> test2 = []() { std::cout << "test2" << std::endl; };
     SCOPED_EXIT(test2);
+    
+    SCOPED_EXIT(test3);
     
     return 0;
 }
