dfaeb7a456be627e56afc8a190c663fca6cc96f3
Yu Sheng Oh <yusheng.oh2@dyson.com>
2023-08-09 22:59:07 +0800

    + Sketch copy-on-write
    ~ update comments.

--- cpp/idioms/copy_on_write.cpp
+++ cpp/idioms/copy_on_write.cpp
@@ -7,16 +7,21 @@
 
 template<std::copyable T>
 class CopyOnWritePtr {
+    // 无法直接约束CopyOnWritePtr类模板为某个concept，只有在实例化时方可约束。
+    //static_assert(std::copyable<CopyOnWritePtr<T>>);
+    //static_assert(std::copyable<std::shared_ptr<T>>);
 public:
     /** */
     CopyOnWritePtr() = default;
     /** */
-    CopyOnWritePtr(const CopyOnWritePtr &robj);
+    CopyOnWritePtr(const CopyOnWritePtr &robj) = default;
     /** */
     template<typename... Ts>
-    CopyOnWritePtr(Ts... ts); //注意：universal absorber when a ctor of certain form is not defined.
+    CopyOnWritePtr(Ts... ts); // 注意：universal absorber when a ctor of certain form is not declared.
     /** */
-    CopyOnWritePtr &operator=(const CopyOnWritePtr &robj);
+    CopyOnWritePtr &operator=(const CopyOnWritePtr &robj) = default;
+    /** */
+    ~CopyOnWritePtr() = default;
     
     /** */
     T *GetMut();
@@ -32,22 +37,10 @@ private:
 };
 
 template<std::copyable T>
-CopyOnWritePtr<T>::CopyOnWritePtr(const CopyOnWritePtr &robj)
-{
-    data_ = robj.data_;
-}
-
-template<std::copyable T>
 template<typename... Ts>
 CopyOnWritePtr<T>::CopyOnWritePtr(Ts... ts)
 {
     data_ = std::make_shared<T>(ts...);
-}
-
-template<std::copyable T>
-CopyOnWritePtr<T> &CopyOnWritePtr<T>::operator=(const CopyOnWritePtr &robj)
-{
-    data_ = robj.data_;
 }
 
 template<std::copyable T>
@@ -75,7 +68,8 @@ int main(int argc, const char *argv[])
 {
     //CopyOnWritePtr<std::string> t0 = "hello world";
     CopyOnWritePtr<std::string> t0;
-    const CopyOnWritePtr<std::string> t1 = t0;
+    //const CopyOnWritePtr<std::string> t1 = t0;
+    std::copyable auto t1 = t0; // 在这里约束CopyOnWritePtr<std::string>为std::copy对象。
     //t0.GetImmut()->at(0);
     //std::cout << "t0:" << t0.GetImmut() << " " << *t0.GetImmut() << std::endl;
     //std::cout << "t1:" << t1.GetImmut() << " " << *t1.GetImmut() << std::endl;
