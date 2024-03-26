57166573d0aaf1d6e0f52e4c5a50ea2eeeaf0bd5
Oh Yu Sheng <humbleoh@gmail.com>
2024-03-26 21:35:19 +0800

    Sketch a simplified version of thread pool.

---
+++ thread_pool_v0.cpp
@@ -0,0 +1,138 @@
+#include <atomic>
+#include <cstddef>
+#include <cassert>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <stop_token>
+#include <thread>
+#include <vector>
+#include <queue>
+
+class thread_pool
+{
+public:
+  ///
+  explicit thread_pool(std::size_t capacity)
+  {
+    assert(capacity >= 1u);
+    threads_.reserve(capacity);
+    for (int i = 0; i < capacity; ++i) {
+      threads_.emplace_back(std::bind_front(&thread_pool::scheduled_run, this));
+      // alternative:
+      //   threads_.emplace_back(std::bind(&thread_pool::scheduled_run, this, std::placeholders::_1))
+      // alternative:
+      //   threads_.emplace_back([this](std::stop_token s) { scheduled_run(s); });
+    }
+  }
+
+  ///
+  ~thread_pool()
+  {
+    stop();
+    join();
+  }
+
+  ///
+  void stop()
+  {
+    for (auto&& t: threads_) {
+      t.request_stop();
+    }
+    condvar_.notify_all();
+  }
+
+  ///
+  void join()
+  {
+    for (auto&& t: threads_) {
+      if (t.joinable()) {
+        t.join();
+      }
+    }
+  }
+
+  /// Wait all pending jobs to be completed.
+  /**
+   * 算是基本操作，等待队列中所有的工作执行完毕
+   * @code
+   * while (!tasks_.empty()) {} // 应该避免 busy-loop
+   * join();
+   * @endcode
+   */
+  void wait()
+  {
+  }
+
+  ///
+  template<typename Fn, typename... Args>
+  auto submit(Fn&& f, Args... args)
+  {
+    using return_type = std::result_of_t<Fn(Args...)>;
+    // 这里使用 std::bind 是因为没法直接使用闭包捕获来完美转发；
+    // 另一个方法是通过 reference wrapper 来实现捕获的完美转发。
+    // std::function 要求 callable 对象是 constructible 的，但
+    // std::packaged_task 并不满足 constructible 的要求，故通过
+    // std::shared_ptr 来满足 constructible 的限制。
+    auto ptask = std::make_shared<std::packaged_task<return_type()>>(
+        std::bind(std::forward<Fn>(f), std::forward<Args>(args)...));
+    auto future = ptask->get_future();
+    auto lock = std::lock_guard {mutex_};
+    tasks_.emplace([ptask]() { (*ptask)(); });
+    condvar_.notify_one();
+    // 返回的 future 不能直接 get()，应当先 wait_for(timeout)。
+    // 使用超时机制是因为工作是投递到队列中，该工作可能不会立刻执行。
+    // 若在执行之前，线程池管理器终止所有线程，造成队列中的工作就不会
+    // 执行返回结果，那么 get() 的后果就是无限等待。
+    return future;
+  }
+
+private:
+  ///
+  void scheduled_run(std::stop_token stop)
+  {
+    while (!stop.stop_requested()) {
+      auto lock = std::unique_lock {mutex_};
+      condvar_.wait(lock, 
+        [this, &stop]() { return !tasks_.empty() || stop.stop_requested(); });
+      if (stop.stop_requested()) {
+        break;
+      }
+
+      auto t = tasks_.front();
+      tasks_.pop();
+      lock.unlock();
+      t();
+    }
+  }
+
+private:
+  std::vector<std::jthread> threads_;
+  std::queue<std::function<void()>> tasks_;
+  std::mutex mutex_;
+  std::condition_variable condvar_;
+};
+
+int main(int argc, char *argv[])
+{
+  thread_pool pool {10};
+  std::atomic_int i {0};
+  int j = 0;
+  pool.submit([&i]() { for (int j = 0; j < 10000; ++j) ++i; });
+  pool.submit([&i]() { for (int j = 0; j < 1000; ++j) ++i; });
+  pool.submit([&i]() { for (int j = 0; j < 1000; ++j) ++i; });
+  pool.submit([&i]() { for (int j = 0; j < 1000; ++j) ++i; });
+  pool.submit([&i]() { for (int j = 0; j < 1000; ++j) ++i; });
+  pool.submit([&i]() { for (int j = 0; j < 1000; ++j) ++i; });
+  pool.submit([&i]() { for (int j = 0; j < 1000; ++j) ++i; });
+  pool.submit([&i]() { for (int j = 0; j < 1000; ++j) ++i; });
+  pool.submit([&i]() { for (int j = 0; j < 1000; ++j) ++i; });
+  pool.submit([&i]() { for (int j = 0; j < 1000; ++j) ++i; });
+  sleep(7); // Workaround for wait()
+  // pool.wait();
+  pool.stop();
+  pool.join();
+  std::cout << i << std::endl;
+  return 0;
+}
